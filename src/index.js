2194bfe104a4b64ad13168399836a6e35c0dfbdc
deksden <deksden@deksden.com>
2019-07-26 16:51:50 +0700

    (WIP) remove mail and wrap services - should be used in userspace, no need for deps here

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import Wrap from './utils/wrap'
-import Mail from 'express-knex-mailer'
-
 export default (app) => {
   return Promise.resolve()
     .then(() => {
-      // configure wrap service:
-      app.wrap = Wrap(app)
-      app.mail = Mail(app)
-
       return app
     })
 }
